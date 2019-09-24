9e9d91b67ce4ba309267f745b4f04a25dbcda225
tehpsalmist <bstewardcodes@gmail.com>
2019-09-24 12:12:44 -0400

    update notifier

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const program = require('commander')
 const evaluateDirectory = require('./src/evaluateDirectory')
 
+const pkg = require('./package.json')
+const updateNotifier = require('update-notifier')
+
 program
   .option('-b --branch <branch>', 'specify a branch of the legacy wrapper repo', str => `#${str}`, '#master')
   .parse(process.argv)
@@ -11,3 +14,7 @@ program
 const { branch } = program
 
 fs.readdir(process.cwd(), { encoding: 'utf8', withFileTypes: true }, evaluateDirectory({ branch }))
+
+const notifier = updateNotifier({ pkg })
+
+notifier.notify('Run npm i -g ZengineHQ/zengine-migrator to update')
